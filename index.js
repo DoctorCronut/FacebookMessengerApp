80564813846fff1539d5ffe7a2e2bef91242ef44
DoctorCronut <ericwzxwu@gmail.com>
2020-06-13 21:41:37 -0700

    handle message code added

--- index.js
+++ index.js
@@ -71,7 +71,20 @@ app.get('/webhook', (req, res) => {
 
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
+    
+  let response;
 
+  // Check if the message contains text
+  if (received_message.text) {    
+
+    // Create the payload for a basic text message
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+    }
+  }  
+  
+  // Sends the response message
+  callSendAPI(sender_psid, response);  
 }
 
 // Handles messaging_postbacks events
