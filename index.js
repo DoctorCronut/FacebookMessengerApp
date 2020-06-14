05170cd8485a339f4efbde3df3f43f83b0ab6f7f
DoctorCronut <ericwzxwu@gmail.com>
2020-06-13 21:34:21 -0700

    installed some things

--- index.js
+++ index.js
@@ -14,20 +14,20 @@ app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
 // Creates the endpoint for our webhook
 app.post('/webhook', (req, res) => {
-    
+
     let body = req.body;
 
     // Checks this is an event from a page subscription
     if (body.object == 'page') {
 
         // Iterates over each entry - there may be multiple if batched
-        body.entry.forEach(function(entry) {
+        body.entry.forEach(function (entry) {
 
             // Gets the message. entry.messaging is an array, but
             // will only ever contian one message, so we get index 0
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
-            
+
             // Get the sender PSID
             let sender_psid = webhook_event.sender.id;
             console.log('Sender PSID: ' + sender_psid);
@@ -35,10 +35,10 @@ app.post('/webhook', (req, res) => {
 
         // Returns a '200 OK' response to all requests
         res.status(200).send('EVENT_RECEIVED');
-     } else {
-         // Returns a '404 Not Found' if event is not from a page subscription
-         res.sendStatus(404);
-     }
+    } else {
+        // Returns a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
+    }
 });
 
 // Adds support for GET requests to our webhook
@@ -83,9 +83,23 @@ function handlePostback(sender_psid, received_postback) {
 function callSendAPI(sender_psid, response) {
     // Construct the message body
     let request_body = {
-        "recipient" : {
+        "recipient": {
             "id": sender_psid
         },
         "message": response
     }
+
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
 }
