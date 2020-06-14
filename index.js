0eaa12ee9f37a45b255e827afc971bda29fdc963
DoctorCronut <ericwzxwu@gmail.com>
2020-06-13 21:45:24 -0700

    added calls to message handler and pb

--- index.js
+++ index.js
@@ -31,6 +31,14 @@ app.post('/webhook', (req, res) => {
             // Get the sender PSID
             let sender_psid = webhook_event.sender.id;
             console.log('Sender PSID: ' + sender_psid);
+
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
+            if (webhook_event.message) {
+            handleMessage(sender_psid, webhook_event.message);        
+            } else if (webhook_event.postback) {
+            handlePostback(sender_psid, webhook_event.postback);
+            }
         });
 
         // Returns a '200 OK' response to all requests
