cc75a761fe9f9ec0ec2829d7c9a07371415c024a
DoctorCronut <ericwzxwu@gmail.com>
2020-06-20 11:05:49 -0700

    check old index.js

--- index.js
+++ index.js
@@ -13,24 +13,6 @@ const
 var listener = app.listen(process.env.PORT || 1337, function () {
     console.log("Your app is listening on port " + listener.address().port);
 
-    if (
-        config.appUrl &&
-        config.verifyToken
-    ) {
-        console.log(
-            "Is this the first time running?\n" +
-            "Make sure to set the both the Messenger profile, persona " +
-            "and webhook by visiting:\n" +
-            config.appUrl +
-            "/profile?mode=all&verify_token=" +
-            config.verifyToken
-        );
-    }
-
-    if (config.pageId) {
-        console.log("Test your app by messaging:");
-        console.log("https://m.me/" + config.pageId);
-    }
 });
 
 // Creates the endpoint for our webhook
