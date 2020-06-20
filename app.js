3efacfb10a72d76354cefcb1d99f56d5dc7b2406
DoctorCronut <ericwzxwu@gmail.com>
2020-06-20 11:13:40 -0700

    check old index.js

--- app.js
+++ app.js
@@ -36,6 +36,38 @@ var listener = app.listen(config.port, function () {
     }
 });
 
+// Creates the endpoint for our webhook
+app.post('/webhook', (req, res) => {
+
+    let body = req.body;
+    // Checks if this is an event from a page subscription
+    if (body.object == 'page') {
+
+        body.entry.forEach(function (entry) {
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
+
+            // if ("read" in webhook_event) {
+            //     return;
+            // }
+
+            // if ("delivery" in webhook_event) {
+            //     return;
+            // }
+
+            // let receiveMessage = new Receive(users[sender_psid], webhook_event);
+            // return receiveMessage.handleMessage();
+        });
+
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+        res.sendStatus(404);
+    }
+});
+
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
 
@@ -44,6 +76,7 @@ app.get('/webhook', (req, res) => {
     let challenge = req.query['hub.challenge'];
 
     if (mode && token) {
+        console.log(config.verifyToken);
         if (mode == 'subscribe' && token == config.verifyToken) {
             console.log('WEBHOOK_VERIFIED');
             res.status(200).send(challenge);
@@ -53,36 +86,5 @@ app.get('/webhook', (req, res) => {
     }
 });
 
-// Creates the endpoint for our webhook
-app.post('/webhook', (req, res) => {
 
-    let body = req.body;
-    console.log("Statement 2 reached!")
-    // Checks if this is an event from a page subscription
-    if (body.object == 'page') {
-        console.log("Statement 3 reached!")
-        res.status(200).send('EVENT_RECEIVED');
-
-        body.entry.forEach(function (entry) {
-            let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
-
-            let sender_psid = webhook_event.sender.id;
-            console.log('Sender PSID: ' + sender_psid);
-
-            if ("read" in webhook_event) {
-                return;
-            }
-
-            if ("delivery" in webhook_event) {
-                return;
-            }
-
-            let receiveMessage = new Receive(users[sender_psid], webhook_event);
-            return receiveMessage.handleMessage();
-        });
-    } else {
-        res.sendStatus(404);
-    }
-});
 
