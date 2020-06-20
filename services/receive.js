1dd1e793aa4e3ae6f06d0cdf01513ae4e686c571
DoctorCronut <ericwzxwu@gmail.com>
2020-06-20 13:30:27 -0700

    check old app.js

--- services/receive.js
+++ services/receive.js
@@ -19,6 +19,7 @@ module.exports = class Receive {
 
         try {
             if (event.message) {
+                let message = event.message;
                 if (message.quick_reply) {
                     responses = this.handleQuickReply();
                 } else if (message.attachments) {
@@ -26,13 +27,12 @@ module.exports = class Receive {
                 } else if (message.text) {
                     responses = this.handleTextMessage();
                 }
-            } else {
-                if (event.postback) {
-                    responses = this.handlePostback();
-                } else if (event.referral) {
-                    responses = this.handleReferral();
-                }
+            } else if (event.postback) {
+                responses = this.handlePostback();
+            } else if (event.referral) {
+                responses = this.handleReferral();
             }
+
         } catch (error) {
             console.error(error);
             responses = {
@@ -248,35 +248,35 @@ module.exports = class Receive {
     static callFBAEventsAPI(senderPsid, eventName) {
         // Construct the message body
         let requestBody = {
-          event: "CUSTOM_APP_EVENTS",
-          custom_events: JSON.stringify([
-            {
-              _eventName: "postback_payload",
-              _value: eventName,
-              _origin: "original_coast_clothing"
-            }
-          ]),
-          advertiser_tracking_enabled: 1,
-          application_tracking_enabled: 1,
-          extinfo: JSON.stringify(["mb1"]),
-          page_id: config.pageId,
-          page_scoped_user_id: senderPsid
+            event: "CUSTOM_APP_EVENTS",
+            custom_events: JSON.stringify([
+                {
+                    _eventName: "postback_payload",
+                    _value: eventName,
+                    _origin: "original_coast_clothing"
+                }
+            ]),
+            advertiser_tracking_enabled: 1,
+            application_tracking_enabled: 1,
+            extinfo: JSON.stringify(["mb1"]),
+            page_id: config.pageId,
+            page_scoped_user_id: senderPsid
         };
-    
+
         // Send the HTTP request to the Activities API
         request(
-          {
-            uri: `${config.mPlatfom}/${config.appId}/activities`,
-            method: "POST",
-            form: requestBody
-          },
-          error => {
-            if (!error) {
-              console.log(`FBA event '${eventName}'`);
-            } else {
-              console.error(`Unable to send FBA event '${eventName}':` + error);
+            {
+                uri: `${config.mPlatfom}/${config.appId}/activities`,
+                method: "POST",
+                form: requestBody
+            },
+            error => {
+                if (!error) {
+                    console.log(`FBA event '${eventName}'`);
+                } else {
+                    console.error(`Unable to send FBA event '${eventName}':` + error);
+                }
             }
-          }
         );
-      }
+    }
 };
