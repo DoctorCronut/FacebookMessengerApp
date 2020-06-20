1ff4aad4eb55deee7f8e46c8af0116ca639806cd
DoctorCronut <ericwzxwu@gmail.com>
2020-06-20 14:50:25 -0700

    check old app.js

--- services/receive.js
+++ services/receive.js
@@ -4,6 +4,7 @@ const
     Curation = require("./curation"),
     Response = require("./response"),
     request = require("request"),
+    config = require("./config"),
     i18n = require("../locales/en_US.json");
 
 module.exports = class Receive {
@@ -138,7 +139,7 @@ module.exports = class Receive {
     handlePayload(payload) {
         console.log("Received Payload:", '${payload} for ${this.user.psid}');
 
-        this.callFBAEventsAPI(this.user.psid, payload);
+        callFBAEventsAPI(this.user.psid, payload);
 
         let response;
 
@@ -229,43 +230,10 @@ module.exports = class Receive {
 
     
 
-    static callFBAEventsAPI(senderPsid, eventName) {
-        // Construct the message body
-        let requestBody = {
-            event: "CUSTOM_APP_EVENTS",
-            custom_events: JSON.stringify([
-                {
-                    _eventName: "postback_payload",
-                    _value: eventName,
-                    _origin: "original_coast_clothing"
-                }
-            ]),
-            advertiser_tracking_enabled: 1,
-            application_tracking_enabled: 1,
-            extinfo: JSON.stringify(["mb1"]),
-            page_id: config.pageId,
-            page_scoped_user_id: senderPsid
-        };
-
-        // Send the HTTP request to the Activities API
-        request(
-            {
-                uri: `${config.mPlatfom}/${config.appId}/activities`,
-                method: "POST",
-                form: requestBody
-            },
-            error => {
-                if (!error) {
-                    console.log(`FBA event '${eventName}'`);
-                } else {
-                    console.error(`Unable to send FBA event '${eventName}':` + error);
-                }
-            }
-        );
-    }
+    
 };
 
-function callSendAPI(requestBody) {
+static function callSendAPI(requestBody) {
     request(
         {
             uri: '${config.mPlatfom}/me/messages',
@@ -278,6 +246,41 @@ function callSendAPI(requestBody) {
         error => {
             if (error) {
                 console.error("Unable to send message:", error);
+            }
+        }
+    );
+}
+
+static function callFBAEventsAPI(senderPsid, eventName) {
+    // Construct the message body
+    let requestBody = {
+        event: "CUSTOM_APP_EVENTS",
+        custom_events: JSON.stringify([
+            {
+                _eventName: "postback_payload",
+                _value: eventName,
+                _origin: "original_coast_clothing"
+            }
+        ]),
+        advertiser_tracking_enabled: 1,
+        application_tracking_enabled: 1,
+        extinfo: JSON.stringify(["mb1"]),
+        page_id: config.pageId,
+        page_scoped_user_id: senderPsid
+    };
+
+    // Send the HTTP request to the Activities API
+    request(
+        {
+            uri: `${config.mPlatfom}/${config.appId}/activities`,
+            method: "POST",
+            form: requestBody
+        },
+        error => {
+            if (!error) {
+                console.log(`FBA event '${eventName}'`);
+            } else {
+                console.error(`Unable to send FBA event '${eventName}':` + error);
             }
         }
     );
