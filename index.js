5f090f26b5c6158b0ff36d9621ee513809502bbd
DoctorCronut <ericwzxwu@gmail.com>
2020-06-20 11:18:13 -0700

    check old index.js

--- index.js
+++ index.js
@@ -37,11 +37,11 @@ app.post('/webhook', (req, res) => {
 
             // Check if the event is a message or postback and
             // pass the event to the appropriate handler function
-            if (webhook_event.message) {
-                handleMessage(sender_psid, webhook_event.message);
-            } else if (webhook_event.postback) {
-                handlePostback(sender_psid, webhook_event.postback);
-            }
+            // if (webhook_event.message) {
+            //     handleMessage(sender_psid, webhook_event.message);
+            // } else if (webhook_event.postback) {
+            //     handlePostback(sender_psid, webhook_event.postback);
+            // }
         });
 
         // Returns a '200 OK' response to all requests
