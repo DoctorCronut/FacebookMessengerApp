8b8f2de6fbe5bb0d4b800d75d146d69d028d6872
DoctorCronut <ericwzxwu@gmail.com>
2020-06-21 16:20:12 -0700

    made some changes

--- services/receive.js
+++ services/receive.js
@@ -169,74 +169,66 @@ module.exports = class Receive {
         //     };
         // }
 
-        callSendAPI(requestBody);
+        console.log(JSON.stringify(requestBody));
+        // let domain = config.mPlatformDomain;
+        request(
+            {
+                uri: "https://graph.facebook.com/v2.6/me/messages",
+                qs: {
+                    access_token: 'EAAJ6dDA8aW8BAKKzLI7e7c9BfuiNkGDixRjKwhJvJZAID64JQG6wcFAn3R2nprOzbEZAFAaZC1F0gybAAmFruyeVhLbTl3ZBQpolwv4ZBnC7SWsgRhZBfEyzYPV1JnrToqFhh3AcbJoyhMUrofPIVl5dqoV2maxZA8wADcs9hLxMgZDZD'
+                },
+                method: "POST",
+                json: requestBody
+            },
+            (error, res, body) => {
+                if (error) {
+                    console.error("Unable to send message:", error);
+                }
+                else {
+                    console.log("message sent!");
+                }
+            }
+        );
     }
 
     firstEntity(nlp, name) {
         return nlp && nlp.entities && nlp.entities[name] && nlp.entities[name][0];
     }
-
-
-
-
 };
 
-function callSendAPI(requestBody) {
-    console.log(JSON.stringify(requestBody));
-    let domain = config.mPlatformDomain;
-    request(
-        {
-            uri: "https://graph.facebook.com/v2.6/me/messages",
-            qs: {
-                access_token: 'EAAJ6dDA8aW8BAKKzLI7e7c9BfuiNkGDixRjKwhJvJZAID64JQG6wcFAn3R2nprOzbEZAFAaZC1F0gybAAmFruyeVhLbTl3ZBQpolwv4ZBnC7SWsgRhZBfEyzYPV1JnrToqFhh3AcbJoyhMUrofPIVl5dqoV2maxZA8wADcs9hLxMgZDZD'
-            },
-            method: "POST",
-            json: requestBody
-        },
-        (error, res, body) => {
-            if (error) {
-                console.error("Unable to send message:", error);
-            }
-            else {
-                console.log("message sent!");
-            }
-        }
-    );
-}
+// function callFBAEventsAPI(senderPsid, eventName) {
+//     // Construct the message body
+//     let domain = config.mPlatformDomain;
+//     let appId = config.appId;
+//     let requestBody = {
+//         event: "CUSTOM_APP_EVENTS",
+//         custom_events: JSON.stringify([
+//             {
+//                 _eventName: "postback_payload",
+//                 _value: eventName,
+//                 _origin: "original_coast_clothing"
+//             }
+//         ]),
+//         advertiser_tracking_enabled: 1,
+//         application_tracking_enabled: 1,
+//         extinfo: JSON.stringify(["mb1"]),
+//         page_id: config.pageId,
+//         page_scoped_user_id: senderPsid
+//     };
 
-function callFBAEventsAPI(senderPsid, eventName) {
-    // Construct the message body
-    let domain = config.mPlatformDomain;
-    let appId = config.appId;
-    let requestBody = {
-        event: "CUSTOM_APP_EVENTS",
-        custom_events: JSON.stringify([
-            {
-                _eventName: "postback_payload",
-                _value: eventName,
-                _origin: "original_coast_clothing"
-            }
-        ]),
-        advertiser_tracking_enabled: 1,
-        application_tracking_enabled: 1,
-        extinfo: JSON.stringify(["mb1"]),
-        page_id: config.pageId,
-        page_scoped_user_id: senderPsid
-    };
-
-    // Send the HTTP request to the Activities API
-    request(
-        {
-            uri: domain + '/' + appId + '/activities',
-            method: "POST",
-            form: requestBody
-        },
-        error => {
-            if (!error) {
-                console.log(`FBA event \'${eventName}\'`);
-            } else {
-                console.error(`Unable to send FBA event \'${eventName}\':` + error);
-            }
-        }
-    );
-}
+//     // Send the HTTP request to the Activities API
+//     request(
+//         {
+//             uri: domain + '/' + appId + '/activities',
+//             method: "POST",
+//             form: requestBody
+//         },
+//         error => {
+//             if (!error) {
+//                 console.log(`FBA event \'${eventName}\'`);
+//             } else {
+//                 console.error(`Unable to send FBA event \'${eventName}\':` + error);
+//             }
+//         }
+//     );
+// }
