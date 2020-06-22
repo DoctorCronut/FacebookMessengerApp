6945dd9371564651a18a9cc9a393bdfd7a7600b0
DoctorCronut <ericwzxwu@gmail.com>
2020-06-22 12:54:16 -0700

    see if this works

--- services/curation.js
+++ services/curation.js
@@ -157,6 +157,7 @@ module.exports = class Curation {
                 if (response.title == i18n.curation.mpg_range[2]) qualities["mpg"] = i18n.curation.mpg_range[2];
                 if (response.title == i18n.curation.mpg_range[3]) qualities["mpg"] = i18n.curation.mpg_range[3];
                 if (response.title == i18n.curation.mpg_range[4]) qualities["mpg"] = i18n.curation.mpg_range[4];
+                console.log(qualities["mpg"]);
                 break;
 
             case "CURATION_RESULT":
